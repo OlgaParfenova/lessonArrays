0a83109c7d13a871c32bcc1585cb05f8f485a6f6
Olga Parfenova <work.parfenova@gmail.com>
2023-02-20 18:38:36 +0300

    The first module is completed

---
+++ script.js
@@ -0,0 +1,31 @@
+// task 1
+
+// let arr1 = [1, 2, 3];
+//
+// console.log(arr1[0]);
+// console.log(arr1[1]);
+// console.log(arr1[2]);
+
+// task 2
+
+// let arr2 = [1, 2, 3];
+//
+// function sumArr(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum +=arr[i];
+//     }
+//     return sum;
+// }
+//
+// console.log(sumArr(arr2));
+
+// task 3
+
+// let arr = ['a', 'b', 'c', 'd'];
+//
+// function toStrWithPlus (arr) {
+//     return arr.join('+');
+// }
+//
+// console.log(toStrWithPlus(arr));
